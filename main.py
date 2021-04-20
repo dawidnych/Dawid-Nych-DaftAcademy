2722f7097e3884670804ee81dfb3bb416421504c
Dawid Nych <dawid.nych@gmail.com>
2021-04-20 21:27:29 +0200

    Task 1.2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 
 app = FastAPI()
 
@@ -6,4 +6,19 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello world!"}
+
+
+@app.get("/{method}")
+def method_endpoint(method, response: Response):
+    tmp = method.upper()
+    if tmp == "GET" or "DELETE" or "OPTION" or "PUT":
+        response.status_code = status.HTTP_200_OK
+        return {"method": f"{method}".upper()}
+    elif tmp == "POST":
+        response.status_code = status.HTTP_201_CREATED
+        return {"method": f"{method}".upper()}
+    else:
+        response.status_code = status.HTTP_406_NOT_ACCEPTABLE
+        return "Wrong method!"
+
 
--- tests.py
+++ tests.py
@@ -9,3 +9,14 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello world!"}
+
+
+def test_method_func(method):
+    response = client.get(f"/{method}")
+    if method.upper() == "GET" or "DELETE" or "OPTION" or "PUT":
+        assert response.status_code == 200
+    elif method.upper() == "POST":
+        assert response.status_code == 201
+    else:
+        assert response.status_code == 406
+    assert response.text == {"method": f"{method}".upper()}
