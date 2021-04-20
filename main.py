f6d2baff15736e25223c911d5487b9361027d719
Dawid Nych <dawid.nych@gmail.com>
2021-04-20 23:50:47 +0200

    Task 1.3

--- main.py
+++ main.py
@@ -10,3 +10,12 @@ app = FastAPI()
 @app.get("/", status_code=200)
 def root():
     return {"message": "Hello world!"}
+
+
+@app.get("/auth")
+def password_check(password: str, password_hash: str, response: Response):
+    h = hashlib.sha512(password.encode('utf-8'))
+    if h.hexdigest() == password_hash:
+        response.status_code = status.HTTP_204_NO_CONTENT
+    elif password == "" or password_hash == "" or h.hexdigest() != password_hash:
+        response.status_code = status.HTTP_401_UNAUTHORIZED
