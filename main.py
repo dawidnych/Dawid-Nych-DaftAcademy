6528c11d90a9038d838682f78daff74b0249f4f0
Dawid Nych <dawid.nych@gmail.com>
2021-04-20 23:48:05 +0200

    Task 1.1

--- main.py
+++ main.py
@@ -1,45 +1,12 @@
 from fastapi import FastAPI, Response, status
 from pydantic import BaseModel
-from datetime import date
+from datetime import date, datetime
+import json
+import hashlib
 
 app = FastAPI()
-
-
-class RegisterIn(BaseModel):
-    name: str
-    surname: str
-
-
-class RegisterOut(BaseModel):
-    id: int
-    name: str
-    surname: str
-    register_date: date
-    vaccination_date: date
 
 
 @app.get("/", status_code=200)
 def root():
     return {"message": "Hello world!"}
-
-
-@app.get("/{method}")
-def method_endpoint(method: str, response: Response):
-    lst = ["GET", "PUT", "OPTIONS", "DELETE"]
-    if method.upper() in lst:
-        response.status_code = status.HTTP_200_OK
-        return {"method": f"{method}".upper()}
-    elif method.upper() == "POST":
-        response.status_code = status.HTTP_201_CREATED
-        return {"method": f"{method}".upper()}
-    else:
-        response.status_code = status.HTTP_406_NOT_ACCEPTABLE
-        return "Wrong method!"
-
-
-
-
-# @app.post("/register")
-# def register_method():
-#     return
-
