614632e751478e6685eb24078e42affb1e7cdf64
Dawid Nych <dawid.nych@gmail.com>
2021-04-20 23:49:21 +0200

    Task 2

--- main.py
+++ main.py
@@ -10,3 +10,14 @@ app = FastAPI()
 @app.get("/", status_code=200)
 def root():
     return {"message": "Hello world!"}
+
+
+@app.get("/{method}")
+def method_endpoint(method: str, response: Response):
+    lst = ["GET", "PUT", "OPTIONS", "DELETE"]
+    if method.upper() in lst:
+        response.status_code = status.HTTP_200_OK
+        return {"method": f"{method}".upper()}
+    elif method.upper() == "POST":
+        response.status_code = status.HTTP_201_CREATED
+        return {"method": f"{method}".upper()}
